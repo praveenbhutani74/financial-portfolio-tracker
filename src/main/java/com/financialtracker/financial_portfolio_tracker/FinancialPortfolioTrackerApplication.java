bba1225375df227b63812b64a06c3510186a44f7
Praveen Bhutani <bhutanipraveen74@gmail.com>
2025-09-17 00:01:32 +0530

    project setup done

---
+++ src/main/java/com/financialtracker/financial_portfolio_tracker/FinancialPortfolioTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.financialtracker.financial_portfolio_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialPortfolioTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialPortfolioTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financialtracker/financial_portfolio_tracker/FinancialPortfolioTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialtracker.financial_portfolio_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialPortfolioTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
